919dc9603a07762abd7b1d3a16197fd24a2080e2
Simon JAILLET <sjaillet@gmail.com>
2017-10-21 01:17:35 +0200

    Upgrade to Kahlan 4.x.

--- kahlan-config.php
+++ kahlan-config.php
@@ -1,41 +1,42 @@
 <?php
 use Lead\Box\Box;
+use Kahlan\Filter\Filters;
 use Chaos\Database\Adapter\MySql;
 use Chaos\Database\Adapter\PostgreSql;
 use Chaos\Database\Adapter\Sqlite;
 
-date_default_timezone_set('UTC');
+Filters::apply($this, 'bootstrap', function($next) {
+    date_default_timezone_set('UTC');
 
-$box = box('chaos.spec', new Box());
+    $box = box('chaos.spec', new Box());
 
-$drivers = PDO::getAvailableDrivers();
+    $drivers = PDO::getAvailableDrivers();
 
-if (in_array('mysql', $drivers)) {
-    $box->factory('source.database.mysql', function() {
-        return new MySql([
-            'database' => 'chaos_test',
-            'username' => 'root',
-            'password' => 'root',
-            'encoding' => 'utf8'
-        ]);
-    });
-}
+    if (in_array('mysql', $drivers)) {
+        $box->factory('source.database.mysql', function() {
+            return new MySql([
+                'database' => 'chaos_test',
+                'username' => 'root',
+                'password' => 'root',
+                'encoding' => 'utf8'
+            ]);
+        });
+    }
 
-if (in_array('pgsql', $drivers)) {
-    $box->factory('source.database.postgresql', function() {
-        return new PostgreSql([
-            'database' => 'chaos_test',
-            'username' => 'postgres',
-            'password' => 'postgres',
-            'encoding' => 'utf8'
-        ]);
-    });
-}
+    if (in_array('pgsql', $drivers)) {
+        $box->factory('source.database.postgresql', function() {
+            return new PostgreSql([
+                'database' => 'chaos_test',
+                'username' => 'postgres',
+                'password' => 'postgres',
+                'encoding' => 'utf8'
+            ]);
+        });
+    }
 
-if (in_array('sqlite', $drivers)) {
-    $box->factory('source.database.sqlite', function() {
-        return new Sqlite();
-    });
-}
-
-?>
+    if (in_array('sqlite', $drivers)) {
+        $box->factory('source.database.sqlite', function() {
+            return new Sqlite();
+        });
+    }
+});
--- kahlan-config.travis.php
+++ kahlan-config.travis.php
@@ -1,44 +1,45 @@
 <?php
 use Lead\Box\Box;
+use Kahlan\Filter\Filters;
 use Chaos\Database\Adapter\MySql;
 use Chaos\Database\Adapter\PostgreSql;
 use Chaos\Database\Adapter\Sqlite;
 
-date_default_timezone_set('UTC');
-
 $commandLine = $this->commandLine();
 $commandLine->option('coverage', 'default', 3);
 
-$box = box('chaos.spec', new Box());
+Filters::apply($this, 'bootstrap', function($next) {
+    date_default_timezone_set('UTC');
 
-$drivers = PDO::getAvailableDrivers();
+    $box = box('chaos.spec', new Box());
 
-if (in_array('mysql', $drivers)) {
-    $box->factory('source.database.mysql', function() {
-        return new MySql([
-            'database' => 'chaos_test',
-            'username' => 'root',
-            'password' => '',
-            'encoding' => 'utf8'
-        ]);
-    });
-}
+    $drivers = PDO::getAvailableDrivers();
 
-if (in_array('pgsql', $drivers)) {
-    $box->factory('source.database.postgresql', function() {
-        return new PostgreSql([
-            'database' => 'chaos_test',
-            'username' => 'postgres',
-            'password' => '',
-            'encoding' => 'utf8'
-        ]);
-    });
-}
+    if (in_array('mysql', $drivers)) {
+        $box->factory('source.database.mysql', function() {
+            return new MySql([
+                'database' => 'chaos_test',
+                'username' => 'root',
+                'password' => '',
+                'encoding' => 'utf8'
+            ]);
+        });
+    }
 
-if (in_array('sqlite', $drivers)) {
-    $box->factory('source.database.sqlite', function() {
-        return new Sqlite();
-    });
-}
+    if (in_array('pgsql', $drivers)) {
+        $box->factory('source.database.postgresql', function() {
+            return new PostgreSql([
+                'database' => 'chaos_test',
+                'username' => 'postgres',
+                'password' => '',
+                'encoding' => 'utf8'
+            ]);
+        });
+    }
 
-?>
+    if (in_array('sqlite', $drivers)) {
+        $box->factory('source.database.sqlite', function() {
+            return new Sqlite();
+        });
+    }
+});
